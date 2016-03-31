f9cfa9e541ebbe41f29e13033041b14fb2b96b2c
Ehsan Akhgari <ehsan@mozilla.com>
2016-03-30 21:00:35 -0400

    Add support for building SpiderMonkey incrementally

--- deps/spidershim/build-spidermonkey.sh
+++ deps/spidershim/build-spidermonkey.sh
@@ -2,5 +2,6 @@
 
 test -d build || mkdir build
 cd build
-../spidermonkey/js/src/configure --disable-shared-js --disable-js-shell
-make -j8
+# First try running Make.  If configure has changed, it will fail, so
+# we'll fall back to configure && make.
+make -j8 || (../spidermonkey/js/src/configure --disable-shared-js --disable-js-shell && make -j8)
