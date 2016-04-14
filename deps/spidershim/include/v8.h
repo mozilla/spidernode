mozilla/spidernode

commit fc9f05d7617cd4a3bb467642570f94386b76e4f4

spidershim: Eliminate some of the chakrashim declarations

deps/spidershim/include/v8.h
@@ -51,10 +51,6 @@
 // header to compile.  We need to replace them with SM based stuff.
 typedef void* JsRef;
 typedef JsRef JsValueRef;
-typedef JsRef JsContextRef;
-typedef enum _JsErrorCode : unsigned int {JsNoError} JsErrorCode;
-JsErrorCode JsAddRef(JsRef, unsigned int*);
-JsErrorCode JsRelease(JsRef, unsigned int*);
 const JsRef JS_INVALID_REFERENCE = NULL;
 
 struct JSRuntime;
@@ -2632,10 +2628,10 @@ void PersistentBase<T>::SetWeakCommon(P* parameter, Callback callback) {
 #if 0
   chakrashim::SetObjectWeakReferenceCallback(val_, callback, parameter,
                                              &_weakWrapper);
-#endif
   if (wasStrong) {
     JsRelease(val_, nullptr);
   }
+#endif
 }
 
 template <class T>

commit e129b77e986c18668c324077a26ff5319e6763f1

spidershim: Fix a build error regressed from fc9f05d7

deps/spidershim/include/v8.h
@@ -1518,7 +1518,9 @@ class V8_EXPORT Object : public Value {
                           PropertyAttribute attribute,
                           Handle<AccessorSignature> signature);
 
+#if 0
   JsErrorCode GetObjectData(struct ObjectData** objectData);
+#endif
   ObjectTemplate* GetObjectTemplate();
 };
 