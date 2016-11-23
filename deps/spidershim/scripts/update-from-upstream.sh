d1657f60f5fa2caae87b0fe4ca40b0d7531a799d
Myk Melez <myk@mykzilla.org>
2016-11-23 14:33:48 -0800

    exclude Python bytecode (.pyc) files from sync

--- deps/spidershim/scripts/update-from-upstream.sh
+++ deps/spidershim/scripts/update-from-upstream.sh
@@ -16,29 +16,29 @@ fi
 
 SM_DIR="$1"
 
-rsync -av --delete "$SM_DIR"/nsprpub spidermonkey/
-rsync -av --delete "$SM_DIR"/mfbt spidermonkey/
-rsync -av --delete "$SM_DIR"/memory spidermonkey/
-rsync -av --delete "$SM_DIR"/mozglue spidermonkey/
-rsync -av --delete "$SM_DIR"/python spidermonkey/
-rsync -av --delete "$SM_DIR"/moz.configure spidermonkey/
-rsync -av --delete "$SM_DIR"/moz.build spidermonkey/
-rsync -av --delete "$SM_DIR"/Makefile.in spidermonkey/
-rsync -av --delete "$SM_DIR"/configure.py spidermonkey/
-rsync -av --delete "$SM_DIR"/build spidermonkey/
-rsync -av --delete "$SM_DIR"/testing/mozbase spidermonkey/testing/
-rsync -av --delete "$SM_DIR"/config spidermonkey/
-rsync -av --delete "$SM_DIR"/intl/icu spidermonkey/intl/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/nsprpub spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/mfbt spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/memory spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/mozglue spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/python spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/moz.configure spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/moz.build spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/Makefile.in spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/configure.py spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/build spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/testing/mozbase spidermonkey/testing/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/config spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/intl/icu spidermonkey/intl/
 test -d spidermonkey/layout/tools/reftest || mkdir -p spidermonkey/layout/tools/reftest
 test -d spidermonkey/dom/bindings || mkdir -p spidermonkey/dom/bindings
 test -d spidermonkey/taskcluster || mkdir -p spidermonkey/taskcluster
-rsync -av --delete "$SM_DIR"/taskcluster/moz.build spidermonkey/taskcluster/
-rsync -av --delete "$SM_DIR"/modules/fdlibm spidermonkey/modules/
-rsync -av --delete "$SM_DIR"/js/src spidermonkey/js/
-rsync -av --delete "$SM_DIR"/js/public spidermonkey/js/
-rsync -av --delete "$SM_DIR"/js/moz.configure spidermonkey/js/
-rsync -av --delete "$SM_DIR"/js/ffi.configure spidermonkey/js/
-rsync -av --delete "$SM_DIR"/.cargo spidermonkey/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/taskcluster/moz.build spidermonkey/taskcluster/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/modules/fdlibm spidermonkey/modules/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/js/src spidermonkey/js/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/js/public spidermonkey/js/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/js/moz.configure spidermonkey/js/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/js/ffi.configure spidermonkey/js/
+rsync -av --delete --exclude='*.pyc' "$SM_DIR"/.cargo spidermonkey/
 
 for patch in `ls spidermonkey-patches/* | sort`; do
   (cd spidermonkey && patch -p1 < "../$patch")
