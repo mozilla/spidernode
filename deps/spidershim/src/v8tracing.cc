5d69574c67086ec42c02c9f2fedb59329e54d10c
Myk Melez <myk@mykzilla.org>
2017-01-30 17:08:10 -0800

    convert v8tracing.cc into a stub

--- deps/spidershim/src/v8tracing.cc
+++ deps/spidershim/src/v8tracing.cc
@@ -1,6 +1,22 @@
-// Copyright 2016 the V8 project authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright Mozilla Foundation. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
 
 #include <assert.h>
 #include <string.h>
@@ -8,33 +24,21 @@
 #include "libplatform/v8-tracing.h"
 
 namespace v8 {
-
-class Isolate;
-
 namespace platform {
 namespace tracing {
 
 TraceConfig* TraceConfig::CreateDefaultTraceConfig() {
-  TraceConfig* trace_config = new TraceConfig();
-  trace_config->included_categories_.push_back("v8");
-  return trace_config;
+  return nullptr;
 }
 
 bool TraceConfig::IsCategoryGroupEnabled(const char* category_group) const {
-  for (auto included_category : included_categories_) {
-    if (strcmp(included_category.data(), category_group) == 0) return true;
-  }
   return false;
 }
 
 void TraceConfig::AddIncludedCategory(const char* included_category) {
-  assert(included_category != NULL && strlen(included_category) > 0);
-  included_categories_.push_back(included_category);
 }
 
 void TraceConfig::AddExcludedCategory(const char* excluded_category) {
-  assert(excluded_category != NULL && strlen(excluded_category) > 0);
-  excluded_categories_.push_back(excluded_category);
 }
 
 void TracingController::Initialize(TraceBuffer* trace_buffer) {
@@ -61,19 +65,16 @@ void JSONTraceWriter::AppendTraceEvent(TraceObject* trace_event) {
 void JSONTraceWriter::Flush() {}
 
 TraceWriter* TraceWriter::CreateJSONTraceWriter(std::ostream& stream) {
-  return new JSONTraceWriter(stream);
+  return nullptr;
 }
 
-TraceBufferChunk::TraceBufferChunk(uint32_t seq) : seq_(seq) {}
-
-void TraceBufferChunk::Reset(uint32_t new_seq) {
-  next_free_ = 0;
-  seq_ = new_seq;
+TraceBufferChunk::TraceBufferChunk(uint32_t seq) : seq_(seq) {
 }
+
+void TraceBufferChunk::Reset(uint32_t new_seq) {}
 
 TraceObject* TraceBufferChunk::AddTraceEvent(size_t* event_index) {
-  *event_index = next_free_++;
-  return &chunk_[*event_index];
+  return nullptr;
 }
 
 }  // namespace tracing
