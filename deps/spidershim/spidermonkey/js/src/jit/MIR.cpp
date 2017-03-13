e9d1439a15bf623905a5fa95eb9c9959680488ec
Myk Melez <myk@mykzilla.org>
2017-03-13 09:52:05 -0700

    Syncing SpiderMonkey from Mozilla upstream revision 3386c6befd1ece93e85207dcc6d8fb98e0c45980

--- deps/spidershim/spidermonkey/js/src/jit/MIR.cpp
+++ deps/spidershim/spidermonkey/js/src/jit/MIR.cpp
@@ -1778,6 +1778,19 @@ MAssertRange::printOpcode(GenericPrinter& out) const
     assertedRange()->dump(out);
 }
 
+void MNearbyInt::printOpcode(GenericPrinter& out) const
+{
+    MDefinition::printOpcode(out);
+    const char* roundingModeStr = nullptr;
+    switch (roundingMode_) {
+      case RoundingMode::Up:                roundingModeStr = "(up)"; break;
+      case RoundingMode::Down:              roundingModeStr = "(down)"; break;
+      case RoundingMode::NearestTiesToEven: roundingModeStr = "(nearest ties even)"; break;
+      case RoundingMode::TowardsZero:       roundingModeStr = "(towards zero)"; break;
+    }
+    out.printf(" %s", roundingModeStr);
+}
+
 const char*
 MMathFunction::FunctionName(Function function)
 {
@@ -2151,6 +2164,15 @@ MRound::trySpecializeFloat32(TempAllocator& alloc)
     MOZ_ASSERT(type() == MIRType::Int32);
     if (EnsureFloatInputOrConvert(this, alloc))
         specialization_ = MIRType::Float32;
+}
+
+void
+MNearbyInt::trySpecializeFloat32(TempAllocator& alloc)
+{
+    if (EnsureFloatInputOrConvert(this, alloc)) {
+        specialization_ = MIRType::Float32;
+        setResultType(MIRType::Float32);
+    }
 }
 
 MTableSwitch*
